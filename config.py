f8b9682b1b05153aeeeb4e590c3c3612c122096b
eyvervalbuena <eyver9720@gmail.com>
2025-07-16 11:17:40 -0500

    templetes - config db

---
+++ config.py
@@ -0,0 +1,15 @@
+DB_USER     = "invitado"
+DB_PASSWORD = "Adso2977369"
+DB_HOST     = "127.0.0.1"
+DB_PORT     = 3306
+DB_NAME     = "softgan_db"
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+URL = (
+    f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}"
+    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+engine = create_engine(URL, echo=True)
+SessionLocal = sessionmaker(bind=engine)
